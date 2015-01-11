b77ba2d01021f501f441381325b2b07a2d7b686c
sugarpeas <laura.kapitzky@gmail.com>
2015-01-11 11:04:17 -0800

    Files for submission

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# Read data from folder, filter for desired dates
+library(dplyr)
+data <- read.csv('household_power_consumption.txt', header=TRUE, sep = ";")
+small_data_2 <- filter(data, Date == '2/2/2007')
+small_data_1 <- filter(data, Date == '1/2/2007')
+small_data <- rbind(small_data_1, small_data_2)
+small_data$Global_active_power <- as.numeric(levels(small_data$Global_active_power))[small_data$Global_active_power]
+
+#Make the plot
+png('plot1.png', width = 480, height = 480, units = "px") #turn on png device
+hist(small_data$Global_active_power, xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power', col = 'red')
+dev.off() #turn off png device
